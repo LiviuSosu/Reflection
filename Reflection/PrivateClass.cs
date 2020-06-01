b380f91f3e6a65240d7c3000ff29e2b20915cc6c
LiviuSosu <sosuliviu@gmail.com>
2020-06-01 20:06:21 +0300

    Implemented refection.

---
+++ Reflection/PrivateClass.cs
@@ -0,0 +1,20 @@
+﻿namespace Reflection
+{
+    public class PrivateClass
+    {
+        public PrivateClass() {
+            subPrivateClass = new SubPrivateClass();
+        }
+
+        private SubPrivateClass subPrivateClass;
+    }
+
+    public class SubPrivateClass
+    {
+        public SubPrivateClass() {
+            privateField = 10;
+        }
+
+        private int privateField;
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var privateClass = new PrivateClass();
+
+
+            var prop = privateClass.GetType().GetField("subPrivateClass", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(privateClass);
+            var prop2 = prop.GetType().GetField("privateField", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            PrivateClass x = MakeACopy(ref privateClass);
+            privateClass = new PrivateClass();
+
+            prop2.SetValue(prop, 101);
+            privateClass = x;
+        }
+
+        static PrivateClass MakeACopy(ref PrivateClass refArgument)
+        {
+            PrivateClass objectToBeReturned = refArgument;
+
+            return objectToBeReturned;
+        }
+    }
+}
